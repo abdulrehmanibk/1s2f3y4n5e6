24d3fb8f8f3aeb04cc2c820a2abdb4f955e80d5c
abdulrehmanibk <abdulrehmanyahya3007@gmail.com>
2024-06-27 16:39:53 +0500

    7th commit

--- src/pages/home/navbar.jsx
+++ src/pages/home/navbar.jsx
@@ -4,22 +4,21 @@ import s1img from '../../image/service.webp';
 import c1img from '../../image/message.webp';
 import t1img from '../../image/tick.webp';
 import h1img from '../../image/home.webp';
-import l1img from '../../image/logo.png'
+import l1img from '../../image/logo5.png'
 const Navbar = () => {
   return (
     <>
     <nav className="nav_container">
-        <div className='nav_logo'>
-            {/* <img src={l1img} alt="" /> */}
-            <span>ENYFS</span>
-            {/* <span>E</span>
-            <span>N</span>
-            <span>Y</span>
-            <span>F</span>
-            <span>S</span> */}
-        </div>
 
+    {/* <ul className='sidebar'>
+        <li><img src={l1img} alt="" /></li>
+        <a href="#services"><li><img src={s1img} alt="" /></li></a>
+        <a href="#contact"><li><img src={c1img} alt="" /></li></a>
+        <a href="#trustedBy"><li><img src={t1img} alt="" /></li></a>
+        <a href="#home"><li><img src={h1img} alt="" /></li></a>
+    </ul> */}
     <ul>
+        <li><img src={l1img} alt="" /></li>
         <a href="#services"><li><img src={s1img} alt="" /></li></a>
         <a href="#contact"><li><img src={c1img} alt="" /></li></a>
         <a href="#trustedBy"><li><img src={t1img} alt="" /></li></a>
